b4fddb72ca907787e4db8627059aa16b20874076
bhaas412 <bhaas412@gmail.com>
2022-10-29 12:47:45 -0400

    Implemented inquirer and DB operation methods on frontend and backend

--- index.js
+++ index.js
@@ -1,10 +1,296 @@
 // Inquirer
 const { prompt } = require("inquirer");
 // console.table
-const cTable = require('console.table');
+require("console.table");
 // Require DB object in order to perform queries via callback
 const db = require('./db');
 
 // TODO: Implement Inquirer prompts for all database operations
 // TODO: User input mapped to function that performs query on DB
 // TODO: Return result back to terminal
+
+// Helper function that initializes Inquirer prompts
+function initPrompts() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Departments",
+          value: "VIEW_DEPARTMENTS"
+        },
+        {
+          name: "View All Roles",
+          value: "VIEW_ROLES"
+        },
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES"
+        },
+        {
+          name: "Add a Department",
+          value: "ADD_DEPARTMENT"
+        },
+        {
+          name: "Add a Role",
+          value: "ADD_ROLE"
+        },
+        {
+          name: "Add an Employee",
+          value: "ADD_EMPLOYEE"
+        },
+        {
+          name: "Update an Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE"
+        },
+        {
+          name: "Exit",
+          value: "EXIT"
+        }
+      ]
+    }
+  ]).then(answer => {
+    let choice = answer.choice;
+    // User input mapped to function that performs query on DB
+    switch (choice) {
+      case "VIEW_DEPARTMENTS":
+        viewDepartments();
+        break;
+      case "VIEW_ROLES":
+        viewRoles();
+        break;
+      case "VIEW_EMPLOYEES":
+        viewEmployees();
+        break;
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "ADD_ROLE":
+        addRole();
+        break;
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
+      case "UPDATE_EMPLOYEE_ROLE":
+        updateEmployeeRole();
+        break;
+      default:
+        exit();
+    }
+  }
+  )
+}
+
+// Helper function that views all deparments
+function viewDepartments() {
+  db.findAllDepartments()
+    .then(([rows]) => {
+      let departments = rows;
+      console.log("\n");
+      console.table(departments);
+    })
+    .then(() => initPrompts());
+}
+
+// Helper function that views all roles
+function viewRoles() {
+  db.findAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      console.log("\n");
+      console.table(roles);
+    })
+    .then(() => initPrompts());
+}
+
+// Helper function that views all employees
+function viewEmployees() {
+  db.findAllEmployees()
+    .then(([rows]) => {
+      let employees = rows;
+      console.log("\n");
+      console.table(employees);
+    })
+    .then(() => initPrompts());
+}
+
+// Helper function that adds a department
+function addDepartment() {
+  prompt([
+    {
+      name: "name",
+      message: "What is the name of the department?"
+    }
+  ])
+    .then(res => {
+      let name = res;
+      db.createDepartment(name)
+        .then(() => console.log(`Added ${name.name} to the database`))
+        .then(() => initPrompts())
+    })
+}
+
+// Helper function that adds a role
+function addRole() {
+  // Find all departments first because of foreign key constraint
+  db.findAllDepartments()
+    .then(([rows]) => {
+      let departments = rows;
+      const departmentChoices = departments.map(({ id, name }) => ({
+        name: name,
+        value: id
+      }));
+
+      prompt([
+        {
+          name: "title",
+          message: "What is the name of the role?"
+        },
+        {
+          name: "salary",
+          message: "What is the salary of the role?"
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "Which department does the role belong to?",
+          choices: departmentChoices
+        }
+      ])
+        .then(role => {
+          db.createRole(role)
+            .then(() => console.log(`Added ${role.title} to the database`))
+            .then(() => initPrompts())
+        })
+    })
+}
+
+// Helper function that adds an employee
+function addEmployee() {
+  prompt([
+    {
+      name: "first_name",
+      message: "What is the employee's first name?"
+    },
+    {
+      name: "last_name",
+      message: "What is the employee's last name?"
+    }
+  ])
+    .then(res => {
+      let firstName = res.first_name;
+      let lastName = res.last_name;
+
+      // Find all first because of foreing key constraint
+      db.findAllRoles()
+        .then(([rows]) => {
+          let roles = rows;
+          const roleChoices = roles.map(({ id, title }) => ({
+            name: title,
+            value: id
+          }));
+
+          prompt({
+            type: "list",
+            name: "roleId",
+            message: "What is the employee's role?",
+            choices: roleChoices
+          })
+            .then(res => {
+              let roleId = res.roleId;
+
+              // Find all managers first because of foreign key constraint
+              db.findAllEmployees()
+                .then(([rows]) => {
+                  let employees = rows;
+                  const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+                    name: `${first_name} ${last_name}`,
+                    value: id
+                  }));
+
+                  managerChoices.unshift({ name: "None", value: null });
+
+                  prompt({
+                    type: "list",
+                    name: "managerId",
+                    message: "Who is the employee's manager?",
+                    choices: managerChoices
+                  })
+                    .then(res => {
+                      let employee = {
+                        manager_id: res.managerId,
+                        role_id: roleId,
+                        first_name: firstName,
+                        last_name: lastName
+                      }
+
+                      db.createEmployee(employee);
+                    })
+                    .then(() => console.log(
+                      `Added ${firstName} ${lastName} to the database`
+                    ))
+                    .then(() => initPrompts())
+                })
+            })
+        })
+    })
+}
+
+// Helper function that updates an employee's role
+function updateEmployeeRole() {
+  db.findAllEmployees()
+    .then(([rows]) => {
+      let employees = rows;
+      const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+      }));
+
+      prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee's role do you want to update?",
+          choices: employeeChoices
+        }
+      ])
+        .then(res => {
+          let employeeId = res.employeeId;
+          db.findAllRoles()
+            .then(([rows]) => {
+              let roles = rows;
+              const roleChoices = roles.map(({ id, title }) => ({
+                name: title,
+                value: id
+              }));
+
+              prompt([
+                {
+                  type: "list",
+                  name: "roleId",
+                  message: "Which role do you want to assign the selected employee?",
+                  choices: roleChoices
+                }
+              ])
+                .then(res => db.updateEmployeeRole(employeeId, res.roleId))
+                .then(() => console.log("Updated employee's role"))
+                .then(() => initPrompts())
+            });
+        });
+    })
+}
+
+// Helper function that exits employee tracker
+function exit() {
+  console.log("Exiting...");
+  process.exit();
+}
+
+// Initializes employee tracker
+function init() {
+  initPrompts();
+}
+
+init();
